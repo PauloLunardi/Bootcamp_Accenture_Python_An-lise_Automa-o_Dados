8ec23ee68bd0e170053b35917900e3733dd3e88b
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-05 17:12:49 -0300

    Add usuarios_new table for user information

--- MariaDB/Create_tables.sql
+++ MariaDB/Create_tables.sql
@@ -19,3 +19,11 @@ CREATE TABLE Bootcamp_Accenture.reservas(
     data DATE COMMENT 'Data da reserva',
     status VARCHAR(255) DEFAULT 'Pendente' COMMENT 'Staus da reserva (Confirmada, Pendente, Cancelada, etc,)'
 );
+
+CREATE TABLE Bootcamp_Accenture.usuarios_new(
+    id INT,
+    nome VARCHAR(255) NOT NULL COMMENT 'Nome do usuario',
+    email VARCHAR(100) NOT NULL UNIQUE COMMENT 'E-mail do usuario',
+    endereco VARCHAR(100) NOT NULL COMMENT 'Endereço do usuário',
+    data_nasc DATE NOT NULL COMMENT 'Data de Nascimento do usuario'
+);
