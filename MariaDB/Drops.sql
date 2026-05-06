412a5ebf92457d2ada1f5097732d1366117362d5
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-06 16:15:53 -0300

    Update Drops.sql

--- MariaDB/Drops.sql
+++ MariaDB/Drops.sql
@@ -2,8 +2,17 @@
 
 "Erro gerado ao dropar uma fk, ao tentar criar outra ela impede, pois cria um registro de index e recria-la antes de remover o registro gerra erro"
 -- Dropar a constraint antiga (não o índice):
+
+-- Para fk_reservas_usuarios
 ALTER TABLE Bootcamp_Accenture.reservas 
 DROP FOREIGN KEY fk_reservas_usuarios;
+-- Para fk_reservas_destinos
+ALTER TABLE Bootcamp_Accenture.reservas 
+DROP FOREIGN KEY fk_reservas_destinos;
 
 -- Dropar o indice contendo registro da fk(erro gerado por uma das tentativas)
+
+-- Para fk_reservas_usuarios
 ALTER TABLE Bootcamp_Accenture.reservas DROP INDEX fk_reservas_usuarios;
+-- Para fk_reservas_destinos
+ALTER TABLE Bootcamp_Accenture.reservas DROP INDEX fk_reservas_destinos;
