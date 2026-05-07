2670f41843dc6be90e5ae82123183cb02b41141f
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-07 15:49:09 -0300

    Enhance JSON file writing with UTF-8 and indentation
    Updated JSON file writing to use UTF-8 encoding and added indentation for readability.

--- Python_Integrations/Read_Json.py
+++ Python_Integrations/Read_Json.py
@@ -8,3 +8,12 @@ dados = {
 
 with open("usuario.json", "w") as arquivo:
   json.dump(dados, arquivo)
+
+# recomendações utilizar UTF-8 e indent 4
+"""import json
+
+dados = {"nome": "Paulo", "idade": 37}
+
+with open("usuario.json", "w", encoding="utf-8") as arquivo:
+    json.dump(dados, arquivo, indent=4)  # indent deixa o JSON mais legível
+"""
