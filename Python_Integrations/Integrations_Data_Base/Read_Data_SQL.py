013727f4582aca1aec04ac8ba9b86e8b56ed79dd
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-11 17:53:01 -0300

    Add Read_Data_SQL.py to read data from SQLite
    This script connects to a SQLite database and retrieves data from the 'usuarios_vip' table.

---
+++ Python_Integrations/Integrations_Data_Base/Read_Data_SQL.py
@@ -0,0 +1,12 @@
+# Lendo os dados
+
+conexao = sqlite3.connect("dados.db")
+
+cursor = conexao.cursor()
+
+cursor.execute("Select * FROM usuarios_vip")
+
+dados = cursor.fetchall()
+
+print(dados)
+
