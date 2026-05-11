d4bcfe0234cf458d038fa8dc30034b6a1cb7f9d9
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-11 18:26:27 -0300

    Rename Read_Data_SQL.py to Read_Data_fetchall_SQL.py

--- Python_Integrations/Integrations_Data_Base/Read_Data_fetchall_SQL.py
+++ Python_Integrations/Integrations_Data_Base/Read_Data_fetchall_SQL.py
@@ -1,14 +1,17 @@
-# Lendo os dados
+# Leitura com fetchall()
+# Lendo todos os resultados de uma vez
+import sqlite3
 
 conexao = sqlite3.connect("dados.db")
+
 cursor = conexao.cursor()
 
 cursor.execute("Select * FROM usuarios_vip")
 
+# retorna uma lista com todas as linhas(em Tuplas)
 dados = cursor.fetchall()
 
 print(dados)
 
 cursor.close()
 conexao.close()
-
