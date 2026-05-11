fa1102e4b00a379ad938b718bd670a9449eb97c9
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-11 16:57:27 -0300

    Implement API data access and display
    Access and print data retrieved from the API in a dictionary format.

---
+++ Python_Integrations/API_Access.py
@@ -0,0 +1,10 @@
+# Acessando os dados que foram lido na API e convertidos anteriormente(API_Requests.py) em dicionario
+
+print("Nome:", dados["name"])
+print("Idade estimada:", dados["age"])
+print("Número de registros:", dados["count"])
+
+# Retorno
+# Nome: Isabela
+# Idade estimada: 42
+# Número de registros: 2190
