9de497f9c3e4110697e423b665accbaca3e53dee
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-11 17:29:05 -0300

    Add data insertion to usuarios_vip table

---
+++ Python_Integrations/Integrations_Data_Base/Data_Insert_SQL.py
@@ -0,0 +1,12 @@
+# Inserindo os dados no banco
+
+import sqlite3
+
+conexao = sqlite3.connect("dados.db")
+
+cursor = conexao.cursor()
+
+cursor.execute(
+    "INSERT INTO usuarios_vip VALUES (?, ?)",
+    ("Lunardi", 37)
+)
