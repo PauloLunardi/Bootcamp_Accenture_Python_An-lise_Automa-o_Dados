8ac9f8e7def9399cdd36a44b685b17b2f432c94e
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-11 18:06:09 -0300

    Close cursor and connection after fetching data

--- Python_Integrations/Integrations_Data_Base/Read_Data_SQL.py
+++ Python_Integrations/Integrations_Data_Base/Read_Data_SQL.py
@@ -1,7 +1,6 @@
 # Lendo os dados
 
 conexao = sqlite3.connect("dados.db")
-
 cursor = conexao.cursor()
 
 cursor.execute("Select * FROM usuarios_vip")
@@ -9,4 +8,7 @@ cursor.execute("Select * FROM usuarios_vip")
 dados = cursor.fetchall()
 
 print(dados)
+
+cursor.close()
+conexao.close()
 
