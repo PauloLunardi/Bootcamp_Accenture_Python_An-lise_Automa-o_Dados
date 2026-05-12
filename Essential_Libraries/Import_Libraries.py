597c149ca563768866b88e0bb01e7c2a9581fff2
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-12 01:53:43 -0300

    Create Import_Libraries.py

---
+++ Essential_Libraries/Import_Libraries.py
@@ -0,0 +1,8 @@
+# importando bibliotecas
+import numpy as np
+import pandas as pd
+import matplotlib.pylot as plt
+
+# importando NumPy (np) para cálculos numéricos, 
+# importando Pandas (pd) para manipulação de dados tabulares
+# importando Matplotlib (plt) para visualização gráfica
