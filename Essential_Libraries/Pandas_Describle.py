0ed775c118fa7f3694a6a71005fa703fe3d96e4b
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-13 18:35:29 -0300

    Create Pandas_Describle.py

---
+++ Essential_Libraries/Pandas_Describle.py
@@ -0,0 +1,19 @@
+# O pandas analisa automaticamente  as colunas numericas no DataFrame e trás algumas estatisticas sobre ela.
+
+import pandas as pd
+
+df.describe()
+
+# Retorno do console
+
+#         preco
+# count	  4.00000
+# mean	  1300.50000
+# std	    2201.47549
+# min	    110.00000
+# 25%	    155.00000
+# 50%	    246.00000
+# 75%	    1391.50000
+# max	    4600.00000
+
+
