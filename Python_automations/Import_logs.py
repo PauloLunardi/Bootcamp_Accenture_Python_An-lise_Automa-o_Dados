925164ea980de7cd862653ca0bda2785cfd8256d
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-21 16:51:49 -0300

    Add logging to Import_logs.py for debugging

---
+++ Python_automations/Import_logs.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("Informação de Depuração")
+logging.info("Processo Iniciado")
+logging.warning("Prossivel problema detectado")
+logging.debug("Erro na execução")
