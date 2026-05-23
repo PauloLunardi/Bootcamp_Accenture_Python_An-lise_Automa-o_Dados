93be97797d63f5f471e59f53d7a58705ced99746
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-23 17:22:58 -0300

    Add choropleth map for life expectancy visualization
    This script creates a choropleth map visualizing life expectancy by country over time using Plotly.

---
+++ Interactive_Graphics/Plotly_4.py
@@ -0,0 +1,20 @@
+fig = px.choropleth(
+  df,
+  locations="iso_alpha",
+  color="lifeExp",
+  hover_name="country",
+  animation_frame="year",
+  color_continuous_scale="Viridis",
+  title="Expectativa de vida no mundo"
+)
+
+fig.show()
+
+# Observação:
+# Este script depende do dataset "tips.csv" disponível no repositório seaborn-data.
+# Para garantir que os gráficos funcionem mesmo após reiniciar a sessão,
+# é necessário carregar novamente os dados com pandas:
+#
+# import pandas as pd
+# url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
+# df = pd.read_csv(url)
