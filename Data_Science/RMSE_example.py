2f8acea4a7739c8eb3b3d5e34722fb52a6ad3515
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-25 19:29:29 -0300

    Create RMSE_example.py

---
+++ Data_Science/RMSE_example.py
@@ -0,0 +1,7 @@
+# O RMSE é simplesmente a raiz quadrada do MSE.
+# Isso permite intepretar o erro na mesma unidade da variável resposta.
+rmse = np.sqrt(mse)
+
+rmse
+
+#... np.float64(69297.71669113015)
