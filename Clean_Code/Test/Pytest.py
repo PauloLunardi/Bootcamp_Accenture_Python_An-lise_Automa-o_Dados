be6dab87bd8c48f65e4c6b0a074c2fc2626f4d53
Paulo Lunardi <lunardi.hb@gmail.com>
2026-05-25 16:30:06 -0300

    Add test function and guidelines in Pytest.py

---
+++ Clean_Code/Test/Pytest.py
@@ -0,0 +1,5 @@
+# Toda vez que usar uma função de teste precisa usar o prefixo "test" 
+# cSe por acaso precise do código em portugues adicione o e no final, ex: teste
+
+def test_soma():
+  assert soma(2,2) == 4
